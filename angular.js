4c120c9e8f02959df15eafc24eba1f0a8d69ee64
frazvi <fatimarazvi@gmail.com>
2016-02-03 16:49:23 -0700

    initial commit

---
+++ angular.js
@@ -0,0 +1,43 @@
+angular.module("myApp", []);
+
+angular
+	.module("myApp")
+	.controller("myController", ['$scope', function($scope) {
+   $scope.days=[];
+
+		$scope.green=false;
+		$scope.red=true;
+		
+
+		$scope.hover = function () {
+			if ($scope.green==true) {
+				$scope.green=false;
+			}
+			else {
+				$scope.green=true
+			}
+			if ($scope.red==true) {
+				$scope.red=false;
+			}
+			else {
+				$scope.red=true
+			}
+		}
+		$scope.bookshelf = ['Book One', 'Book Two', 'Book Three'];
+
+		$scope.bookSubmission = function (extraBook) {
+			$scope.bookshelf.push(extraBook);
+			$scope.extraBook = ''
+		}
+
+		$scope.days = [
+	{"Day": 'Monday'},
+	{"Day": 'Tuesday'},
+	{"Day": 'Wednesday'},
+	{"Day": 'Thursday'},
+	{"Day": 'Friday'},
+	{"Day": 'Saturday'},
+	{"Day": 'Sunday'}
+];
+	}])
+
